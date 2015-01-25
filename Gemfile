22056884d3ad2791f9c9a757aeefe2202665da95
Sergey <biginf.com@gmail.com>
2015-01-25 17:54:20 +0300

    без imagemagic

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails', '4.0.4'
 
 gem 'haml', '~> 4.0.5'
 gem 'simple_form', '~> 3.0.2'
+gem 'paperclip', '~> 4.2.1'
+gem 'bootstrap-sass', '~> 3.3.3'
+
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ RecipeBox::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin/"
 end
